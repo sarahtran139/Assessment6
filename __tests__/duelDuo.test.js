c1219b2b6aeb1ecc1eff6c1440ef5c6fb5279d09
sarahtran139@gmail.com <sarahtran139@gmail.com>
2023-07-30 19:11:10 -0400

    assessment6

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -25,11 +25,11 @@ describe("Duel Duo tests", () => {
   test("clicking an 'Add to Duo' button displays the div with id = 'player-duo'", async () => {
     await driver.get("http://localhost:8000");
     await driver.findElement(By.id("draw")).click();
-    await driver.wait(until.elementLocated(By.className("bot-btn")), 5000); // Wait up to 5 seconds for the "Add to Duo" buttons to appear
-    // Let's assume we select the first bot to add to the player's duo
+    await driver.wait(until.elementLocated(By.className("bot-btn")), 5000); 
+
     const addToDuoButtons = await driver.findElements(By.className("bot-btn"));
     await addToDuoButtons[0].click();
-    await driver.wait(until.elementLocated(By.id("player-duo")), 5000); // Wait up to 5 seconds for the "player-duo" div to appear
+    await driver.wait(until.elementLocated(By.id("player-duo")), 5000); 
     const playerDuoDiv = await driver.findElement(By.id("player-duo"));
     expect(await playerDuoDiv.isDisplayed()).toBe(true);
   });
